40b78bde307424c881d816ed9c44a88b0cfa07d3
donisdonatas <donisdonatas@gmail.com>
2022-11-17 19:48:22 +0200

    Changes ?? Negaliu Pull daryti.

--- Coop_task/Var_Sum.cs
+++ Coop_task/Var_Sum.cs
@@ -14,6 +14,7 @@ namespace Coop_task
         float suma;
         float vidurkis;
 
+<<<<<<< HEAD
         public void getIzambine()
         {
             //========================
@@ -23,6 +24,13 @@ namespace Coop_task
             int a = 10;
             int b = 15;
             this.izambine = (float)Math.Sqrt((float)Math.Pow(a, 2) + (float)Math.Pow(b, 2));
+=======
+        public void CountEarthArea () 
+        {
+            float radius = 6371;
+
+            this.zemesPlotas = 4 * (float)Math.PI * (float)Math.Pow(radius, 2.0);
+>>>>>>> 5a9faf1a7544b9884aab70b0e4c58721cb874ac1
         }
 
         public void writeIzambine()
