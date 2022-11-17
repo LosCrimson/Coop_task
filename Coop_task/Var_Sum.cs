373fe4b061e945947a8da6468e1af4d078115672
donisdonatas <donisdonatas@gmail.com>
2022-11-17 19:33:34 +0200

    Izambines skaičiavimo algoritmas

--- Coop_task/Var_Sum.cs
+++ Coop_task/Var_Sum.cs
@@ -14,7 +14,20 @@ namespace Coop_task
         float suma;
         float vidurkis;
 
+        public void getIzambine()
+        {
+            //========================
+            // Įžambinės skaičiavimas
+            //=======================
 
+            int a = 10;
+            int b = 15;
+            this.izambine = (float)Math.Sqrt((float)Math.Pow(a, 2) + (float)Math.Pow(b, 2));
+        }
 
+        public void writeIzambine()
+        {
+            Console.WriteLine("Trikampio ižambinės ilgis: {0}", this.izambine);
+        }
     }
 }
